4c660da090c6288969cec488d04619f235a686f6
Alireza Rahimi <rahimialireza.az@gmail.com>
2022-01-12 17:15:34 +0330

    add two method to evaluate hmm

--- src/test.py
+++ src/test.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from hmmlearn import hmm
+from sklearn.metrics import accuracy_score
+from itertools import permutations
 
 
-def plot_data(x):
+def plot_data(x, user):
+    x = x.reshape(-1, 100)[user]
     time = np.arange(0, x.shape[0])
     plt.plot(time, x)
     plt.show()
 
 
-def plot_data_color(x, y):
+def plot_data_color(x, y, user):
+    x = x.reshape(-1, 100)[user]
+    y = y.reshape(-1, 100)[user]
     fig, ax = plt.subplots()
     time = np.arange(0, x.shape[0])
     ax.plot(time, x)
@@ -49,6 +54,7 @@ def plot_data_color(x, y):
 
 def generate_data(length, dim):
     x = []
+    y = []
     transmat = np.array([[0.9, 0.95, 1],
                          [0.05, 0.95, 1],
                          [0.05, 0.1, 1]])
@@ -68,8 +74,10 @@ def generate_data(length, dim):
                 value = np.random.normal(8, 0.5, 1)
                 last = 2
             x.append(value)
+            y.append(last)
     x = np.array(x)
-    return x
+    y = np.array(y)
+    return x, y
 
 
 def fit_model(x, length):
@@ -81,10 +89,34 @@ def fit_model(x, length):
     return y
 
 
+def evaluate_model(y_pred, y_true, class_count):
+    result_map = {}
+    for i in range(class_count):
+        a = y_true == i
+        b = y_pred[a]
+        result = np.unique(b, return_counts=True)
+        result_map[i] = result[0][result[1].argmax()]
+    y_pred = np.vectorize(result_map.get)(y_pred)
+    print(accuracy_score(y_true, y_pred))
+
+
+def evaluate_model_two(y_pred, y_true, class_count):
+    perms = permutations(range(class_count))
+    results = []
+    for permutation in perms:
+        my_map = {}
+        for i in range(class_count):
+            my_map[i] = permutation[i]
+        temp_y_pred = np.vectorize(my_map.get)(y_pred)
+        results.append(accuracy_score(y_true, temp_y_pred))
+    print(max(results))
+
+
 if __name__ == '__main__':
-    data = generate_data(100, 100)
-    user_data = data.reshape(-1, 100)
-    plot_data(user_data[4])
-    pred = fit_model(data, np.full_like(100, 100))
-    user_pred = pred.reshape(-1, 100)
-    plot_data_color(user_data[4], user_pred[4])
+    user_to_plot = 400
+    data, label = generate_data(1000, 100)
+    # plot_data(data, user_to_plot)
+    pred = fit_model(data, np.full_like(1000, 100))
+    # plot_data_color(data, pred, user_to_plot)
+    # evaluate_model(pred, label, 3)
+    evaluate_model_two(pred, label, 3)
