703bed85a064851b40619a46db09b16c73e2de81
Alireza Rahimi <rahimialireza.az@gmail.com>
2022-01-25 17:29:14 +0330

    update requirements.txt add cpp file that runs algorithm add sh file to compile cpp after editing it

---
+++ src/my_compiler.sh
@@ -0,0 +1,1 @@
+c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) _hmmc.cpp -o /home/alireza/projects/python/HMM/venv/lib/python3.8/site-packages/hmmlearn/_hmmc.cpython-38-x86_64-linux-gnu.so
