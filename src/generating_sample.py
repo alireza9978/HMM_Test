107ac1e828ae361c27563a5289322539afe3e628
Alireza Rahimi <rahimialireza.az@gmail.com>
2022-01-28 14:22:51 +0330

    insert nan to data try to fit hmm model on data with nan

--- src/generating_sample.py
+++ src/generating_sample.py
@@ -1,21 +1,29 @@
 import numpy as np
+
+from src.evaluation import evaluate_model_two
 from visualization import plot_data_color
 from hmmlearn import hmm
 
 np.random.seed(42)
 
 model = hmm.GaussianHMM(n_components=3, covariance_type="full")
+model.startprob_ = np.array([0.33, 0.33, 0.34])
+model.transmat_ = np.array([[0.9, 0.05, 0.05], [0.05, 0.9, 0.05], [0.05, 0.05, 0.9]])
+model.means_ = np.array([[0.0], [4.0], [8.0]])
+model.covars_ = np.array([[[0.5]], [[0.5]], [[0.5]]])
 
-model.startprob_ = np.array([0.6, 0.3, 0.1])
-
-model.transmat_ = np.array([[0.7, 0.2, 0.1],
-                            [0.3, 0.5, 0.2],
-                            [0.3, 0.3, 0.4]])
-
-model.means_ = np.array([[0.0, 0.0], [3.0, -3.0], [5.0, 10.0]])
-
-model.covars_ = np.tile(np.identity(2), (3, 1, 1))
-
+# generate data with defined hmm model
 X, Z = model.sample(100)
-
 plot_data_color(X, Z)
+
+# add nan to data
+temp_indexes = np.random.choice(np.arange(X.shape[0]), 10)
+X[temp_indexes] = np.nan
+new_x = np.array(X, copy=True)
+
+# trying to learn a new model with data that contains nan
+new_model = hmm.GaussianHMM(n_components=3, covariance_type="full")
+new_model.fit(new_x)
+y = new_model.predict(new_x)
+y = np.array(y)
+evaluate_model_two(y, Z, 3)
